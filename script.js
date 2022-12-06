f83bb1ed8ac406c040f31bd94c8c5eeed3fce850
Serhii Vinokhodov <vinokhodova.nadin@gmail.com>
2022-12-06 21:11:50 +0100

    add task21

--- script.js
+++ script.js
@@ -34,22 +34,19 @@ let tuesday = [
 
 const newMonday = monday
     .map(function (monday) {
-        monday[1] =  monday[1] / 60;
-        return monday[1];
-    }
-    )
+        monday[1] = monday[1] / 60;
+        return monday;
+    })
 
     .filter(function (monday) {
         return monday[1] > 2;
-    }
-    )
+    })
 
     .map(function (monday) {
         let checkAmount = (monday[1] * amount);
         monday.push(checkAmount);
         return monday;
-    }
-    )
+    })
 
 console.log(newMonday);
 
@@ -57,21 +54,26 @@ console.log(newMonday);
 
 let newTuesday = tuesday
     .map(function (tuesday) {
-        tuesday[1] =  tuesday[1] / 60;
-        return tuesday[1];
+        tuesday[1] = tuesday[1] / 60;
+        return tuesday;
     }
     )
-
     .filter(function (tuesday) {
         return tuesday[1] > 2;
-    }
-    )
+    })
     .map(function (tuesday) {
         let checkAmount = tuesday[1] * amount;
         tuesday.push(checkAmount);
         return tuesday;
-    }
-    )
+    })
+    .map(function (tuesday) {
+        return `<tr>
+        <td>Task name: ${tuesday[0]}</td>
+        <td>Taks duration: ${tuesday[1]} hours</td>
+        <td>Task amount: $${tuesday[2]}</td>
+        </tr>`
+    })
 
 
-console.log(newTuesday);
+    document.write(`<table>${newTuesday.join("  ")}</table>`)
+    //console.log(newTuesday);
